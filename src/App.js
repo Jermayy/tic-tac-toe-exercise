fbf049518c76b47b4ab1ba5775b6e157377e9759
Jermayy <jeremyVDLRC@gmail.com>
2023-01-04 21:58:23 +1100

    add eslint functionality

--- src/App.js
+++ src/App.js
@@ -1,46 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
 
 function App() {
   return (
-    <div className="App" 
-    style={{ 
-      display: 'flex',
-      alignItems: 'center', 
-      justifyContent: 'center',
-      height: '100vh',
-      width: '100vw'}}>
-       <TicTacToe/>
+    <div
+      className="App"
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        width: '100vw',
+      }}
+    >
+      <TicTacToe />
     </div>
   );
 }
 
-
-function TicTacToe(){
+function TicTacToe() {
   return (
-  <div 
-    style={{
-      display:'grid',
-      gridTemplateColumns:'auto auto auto',
-      gridTemplateRows: 'auto auto auto',
-      columnGap: '10px',
-      rowGap: '10px',
-    }}
-  >
-      <Square/>
-      <Square/>
-      <Square/>
-      <Square/>
-      <Square/>
-      <Square/>
-      <Square/>
-      <Square/>
-      <Square/>
-  </div>
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'auto auto auto',
+        gridTemplateRows: 'auto auto auto',
+        columnGap: '10px',
+        rowGap: '10px',
+      }}
+    >
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+    </div>
   );
 }
 
-function Square(){
+function Square() {
   return (
     <div
       style={{
@@ -49,23 +52,22 @@ function Square(){
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
-      }} 
+      }}
     >
       Square
     </div>
   );
 }
 
-function Circle(){
-  return <div>Circle</div>
+function Circle() {
+  return <div>Circle</div>;
 }
 
-function Cross(){
-  return <div>Cross</div>
+function Cross() {
+  return <div>Cross</div>;
 }
-function Result(){
-  return <div>Cross</div>
+function Result() {
+  return <div>Cross</div>;
 }
-
 
 export default App;
