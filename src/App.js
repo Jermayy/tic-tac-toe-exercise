230d8957bfafba65e31add414703edcc68b69a4a
Jermayy <jeremyVDLRC@gmail.com>
2023-01-04 19:30:57 +1100

    create project and add squares to grid

--- src/App.js
+++ src/App.js
@@ -3,23 +3,69 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" 
+    style={{ 
+      display: 'flex',
+      alignItems: 'center', 
+      justifyContent: 'center',
+      height: '100vh',
+      width: '100vw'}}>
+       <TicTacToe/>
     </div>
   );
 }
+
+
+function TicTacToe(){
+  return (
+  <div 
+    style={{
+      display:'grid',
+      gridTemplateColumns:'auto auto auto',
+      gridTemplateRows: 'auto auto auto',
+      columnGap: '10px',
+      rowGap: '10px',
+    }}
+  >
+      <Square/>
+      <Square/>
+      <Square/>
+      <Square/>
+      <Square/>
+      <Square/>
+      <Square/>
+      <Square/>
+      <Square/>
+  </div>
+  );
+}
+
+function Square(){
+  return (
+    <div
+      style={{
+        width: '110px',
+        height: '110px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }} 
+    >
+      Square
+    </div>
+  );
+}
+
+function Circle(){
+  return <div>Circle</div>
+}
+
+function Cross(){
+  return <div>Cross</div>
+}
+function Result(){
+  return <div>Cross</div>
+}
+
 
 export default App;
