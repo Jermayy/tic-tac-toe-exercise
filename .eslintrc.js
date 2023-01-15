9974765a3638dab036222d265f455720a48c4e86
Jermayy <jeremyVDLRC@gmail.com>
2023-01-15 20:52:36 +1100

    tic tac toe game now works

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,8 @@ module.exports = {
   ],
   rules: {
     indent: ['error', 2],
+    'react/prop-types': [0],
     'react/jsx-filename-extension': [0, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-no-bind': [0],
   },
 };
